55ef61d2e039ebe6079e65e507ef96d05a54e61c
Ludwika Malinowska <malinowska.L@wp.pl>
2021-03-31 12:23:23 +0200

    SPIS-8 | Poprawienie komponentu AnkieterLogIn, dodanie PropTypes

--- src/components/AnkieterLogIn/AnkieterLogIn.js
+++ src/components/AnkieterLogIn/AnkieterLogIn.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { Button, ButtonGroup, FormLabel, Input } from "@chakra-ui/react"
+import PropTypes from 'prop-types';
 
-const AnkieterLogIn = (props) => {
+const AnkieterLogIn = ({login, passwd, change}) => {
     return (
       <div className={"Ankieterlogin"}>
-        <label>Login: </label>
-        <input type="text" value={props.login} onChange={props.change} />
-        <label>Hasło: </label>
-        <input type="password" value={props.passwd} onChange={props.change} />
-        <button>Zaloguj</button>
+        <FormLabel>Login: </FormLabel>
+        <Input type="text" value={login} onChange={change} />
+        <FormLabel>Hasło: </FormLabel>
+        <Input type="password" value={passwd} onChange={change} />
+        <Button>Zaloguj</Button>
       </div>
     );
-  };
+};
+
+AnkieterLogIn.propTypes = {
+      login: PropTypes.string,
+      passwd: PropTypes.string,
+}
   
-  export default AnkieterLogIn;
+export default AnkieterLogIn;
