d12705491cc0b8144a03c03aae4da21595f68b71
LudwikaMalinowska <LudwikaMalinowska@users.noreply.github.com>
2021-03-31 11:18:16 +0000

    Prettified Code!

--- src/components/AnkieterLogIn/AnkieterLogIn.js
+++ src/components/AnkieterLogIn/AnkieterLogIn.js
@@ -1,23 +1,22 @@
 import React from "react";
-import { Button, ButtonGroup, FormLabel, Input } from "@chakra-ui/react"
-import PropTypes from 'prop-types';
+import { Button, ButtonGroup, FormLabel, Input } from "@chakra-ui/react";
+import PropTypes from "prop-types";
 
-const AnkieterLogIn = ({login, passwd, change}) => {
-    return (
-      <div className={"Ankieterlogin"}>
-        <FormLabel>Login: </FormLabel>
-        <Input type="text" value={login} onChange={change} />
-        <FormLabel>Hasło: </FormLabel>
-        <Input type="password" value={passwd} onChange={change} />
-        <Button>Zaloguj</Button>
-      </div>
-    );
+const AnkieterLogIn = ({ login, passwd, change }) => {
+  return (
+    <div className={"Ankieterlogin"}>
+      <FormLabel>Login: </FormLabel>
+      <Input type="text" value={login} onChange={change} />
+      <FormLabel>Hasło: </FormLabel>
+      <Input type="password" value={passwd} onChange={change} />
+      <Button>Zaloguj</Button>
+    </div>
+  );
 };
 
 AnkieterLogIn.propTypes = {
-      login: PropTypes.string,
-      passwd: PropTypes.string,
-}
-  
-export default AnkieterLogIn;
+  login: PropTypes.string,
+  passwd: PropTypes.string,
+};
 
+export default AnkieterLogIn;
--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,16 +1,16 @@
-import React, {Component} from "react";
-import {Link, withRouter} from "react-router-dom"
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 const LogIn = (props) => {
-        return (
-            <div className="logInButtons">
-                <Button>
-                    <Link to ="/login/ankieter">Zaloguj jako ankieter</Link>
-                </Button>
-                <Button>Zaloguj jako administrator</Button>
-            </div>      
-        );
+  return (
+    <div className="logInButtons">
+      <Button>
+        <Link to="/login/ankieter">Zaloguj jako ankieter</Link>
+      </Button>
+      <Button>Zaloguj jako administrator</Button>
+    </div>
+  );
 };
 
 export default LogIn;
