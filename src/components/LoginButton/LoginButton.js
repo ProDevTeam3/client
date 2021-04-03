fdd663ab5610c7350392b102d54252446fb45eeb
Ludwika Malinowska <malinowska.L@wp.pl>
2021-04-03 12:25:19 +0200

    SPIS-78 | Utworzenie Login Buttona do Auth0

---
+++ src/components/LoginButton/LoginButton.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@chakra-ui/react";
+import { LoginPageStrings } from "../../constants/strings";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+  return (
+
+    <Button colorScheme="teal" size="lg"
+    className="btn btn-primary btn-block"
+    onClick={() => loginWithRedirect()} 
+    >
+            {LoginPageStrings.loginButtonText}
+    </Button>
+    
+  );
+};
+
+export default LoginButton;
