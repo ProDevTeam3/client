d4d93b2554b684d09e4542f5bb5f3ab724c85802
pauljackals <pauljackals@users.noreply.github.com>
2021-04-03 16:56:23 +0000

    Prettified Code!

--- src/components/Form/Fields/Fields.js
+++ src/components/Form/Fields/Fields.js
@@ -1,29 +1,29 @@
-import formData from '../../../constants/formData'
-import {Field} from 'formik'
+import formData from "../../../constants/formData";
+import { Field } from "formik";
 
 export default Fields = () => {
-    return (
-        <>
-            <Field as='select' name="voivodeship" id="voivodeship">
-                {formData.voivodeships.map(voivodeship =>
-                    <option value={voivodeship}>{voivodeship}</option>
-                )}
-            </Field>
-            <Field as='select' name="education" id="education">
-                {formData.education.map(educationLevel =>
-                    <option value={educationLevel}>{educationLevel}</option>
-                )}
-            </Field>
-            <Field as='select' name="marital" id="marital">
-                {formData.marital.map(maritalStatus =>
-                    <option value={maritalStatus}>{maritalStatus}</option>
-                )}
-            </Field>
-            <Field as='select' name="sex" id="sex">
-                {formData.sex.map(sexOption =>
-                    <option value={sexOption}>{sexOption}</option>
-                )}
-            </Field>
-        </>
-    )
-}
+  return (
+    <>
+      <Field as="select" name="voivodeship" id="voivodeship">
+        {formData.voivodeships.map((voivodeship) => (
+          <option value={voivodeship}>{voivodeship}</option>
+        ))}
+      </Field>
+      <Field as="select" name="education" id="education">
+        {formData.education.map((educationLevel) => (
+          <option value={educationLevel}>{educationLevel}</option>
+        ))}
+      </Field>
+      <Field as="select" name="marital" id="marital">
+        {formData.marital.map((maritalStatus) => (
+          <option value={maritalStatus}>{maritalStatus}</option>
+        ))}
+      </Field>
+      <Field as="select" name="sex" id="sex">
+        {formData.sex.map((sexOption) => (
+          <option value={sexOption}>{sexOption}</option>
+        ))}
+      </Field>
+    </>
+  );
+};
