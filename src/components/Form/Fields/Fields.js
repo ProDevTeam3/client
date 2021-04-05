87195da1e6ce862ec6c2d8862f9ff27cce55b6a9
Pawel Sakaluk <paul.jackals@gmail.com>
2021-04-05 20:11:12 +0200

    SPIS-34 | dane do formularza poprawka

--- src/components/Form/Fields/Fields.js
+++ src/components/Form/Fields/Fields.js
@@ -1,29 +1,61 @@
+<<<<<<< HEAD
 import formData from '../../../constants/formData'
-import {Field} from 'formik'
+import {Select} from "@chakra-ui/react"
 
 export default Fields = () => {
     return (
         <>
-            <Field as='select' name="voivodeship" id="voivodeship">
+            <Select name="voivodeship" id="voivodeship" placeholder="Województwo">
                 {formData.voivodeships.map(voivodeship =>
                     <option value={voivodeship}>{voivodeship}</option>
                 )}
-            </Field>
-            <Field as='select' name="education" id="education">
+            </Select>
+            <Select name="education" id="education" placeholder="Wykształcenie">
                 {formData.education.map(educationLevel =>
                     <option value={educationLevel}>{educationLevel}</option>
                 )}
-            </Field>
-            <Field as='select' name="marital" id="marital">
+            </Select>
+            <Select name="marital" id="marital" placeholder="Stan cywilny">
                 {formData.marital.map(maritalStatus =>
                     <option value={maritalStatus}>{maritalStatus}</option>
                 )}
-            </Field>
-            <Field as='select' name="sex" id="sex">
+            </Select>
+            <Select name="sex" id="sex" placeholder="Płeć">
                 {formData.sex.map(sexOption =>
                     <option value={sexOption}>{sexOption}</option>
                 )}
-            </Field>
+            </Select>
         </>
     )
 }
+=======
+import formData from "../../../constants/formData";
+import { Field } from "formik";
+
+export default Fields = () => {
+  return (
+    <>
+      <Field as="select" name="voivodeship" id="voivodeship">
+        {formData.voivodeships.map((voivodeship) => (
+          <option value={voivodeship}>{voivodeship}</option>
+        ))}
+      </Field>
+      <Field as="select" name="education" id="education">
+        {formData.education.map((educationLevel) => (
+          <option value={educationLevel}>{educationLevel}</option>
+        ))}
+      </Field>
+      <Field as="select" name="marital" id="marital">
+        {formData.marital.map((maritalStatus) => (
+          <option value={maritalStatus}>{maritalStatus}</option>
+        ))}
+      </Field>
+      <Field as="select" name="sex" id="sex">
+        {formData.sex.map((sexOption) => (
+          <option value={sexOption}>{sexOption}</option>
+        ))}
+      </Field>
+    </>
+  );
+};
+>>>>>>> d4d93b2554b684d09e4542f5bb5f3ab724c85802
