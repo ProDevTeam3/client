2459570e7c8f1e0f47a3d2e8689b33882f085c4b
KacperKurz <KacperKurz@users.noreply.github.com>
2021-04-06 17:18:59 +0000

    Prettified Code!

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import AuthRoute from "./components/AuthRoute/AuthRoute";
 import LogIn from "./components/LogIn/LogIn";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import {useAuth0} from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const {isAuthenticated} = useAuth0();
+  const { isAuthenticated } = useAuth0();
   return (
     <ChakraProvider>
       <BrowserRouter>
@@ -18,10 +18,10 @@ function App() {
               <Redirect to="/login" />
             </Route>
             <AuthRoute
-                authenticated={isAuthenticated}
-                children={AnkieterLogIn}
-                exact={true}
-                path={"/login/ankieter"}
+              authenticated={isAuthenticated}
+              children={AnkieterLogIn}
+              exact={true}
+              path={"/login/ankieter"}
             />
             <Route exact path="/login" component={LogIn} />
           </Switch>
--- src/components/AuthRoute/AuthRoute.js
+++ src/components/AuthRoute/AuthRoute.js
@@ -1,20 +1,15 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
-const AuthRoute = ({
-                       authenticated,
-                       children,
-                       exact,
-                       path,
-                   }) => {
-    return (
-        <Route
-            exact={exact}
-            path={path}
-            render={(location) =>
-                authenticated ? children : <Redirect to="/login" />
-            }
-        />
-    );
+const AuthRoute = ({ authenticated, children, exact, path }) => {
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      render={(location) =>
+        authenticated ? children : <Redirect to="/login" />
+      }
+    />
+  );
 };
 export default AuthRoute;
