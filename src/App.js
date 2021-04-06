6c865df883a2b21ccf6d354117c666906f979a78
Kacper Kurz <kacperkurz@protonmail.com>
2021-04-06 13:37:09 +0200

    SPIS-70 przeniesiono logikę do osobnego komponentu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
+import AuthRoute from "./components/AuthRoute/AuthRoute";
 import LogIn from "./components/LogIn/LogIn";
-import AnkieterLogIn from "./components/AnkieterLogIn/AnkieterLogIn";
-import { useAuth0 } from "@auth0/auth0-react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-  const {isAuthenticated} = useAuth0();
   return (
     <ChakraProvider>
       <BrowserRouter>
@@ -16,9 +14,7 @@ function App() {
             <Route exact path="/">
               <Redirect to="/login" />
             </Route>
-            // TODO: zmienić komponent/ścieżkę na formularz
-            // TODO: zmienić redirect w auth0
-            <Route path="/login/ankieter" component={isAuthenticated?AnkieterLogIn:LogIn} />
+            <Route exact path="/login/ankieter" component={AuthRoute()}/>
             <Route exact path="/login" component={LogIn} />
           </Switch>
         </div>
