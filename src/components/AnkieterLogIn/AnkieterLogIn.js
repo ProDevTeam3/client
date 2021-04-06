a4fa6e758dc3c4542347c4e40e404a7e69b1ba3b
Marek Połom <marekpolom22@gmail.com>
2021-04-06 13:20:28 +0200

    SPIS-14 | dodanie podstawowej logiki logowania

--- src/components/AnkieterLogIn/AnkieterLogIn.js
+++ src/components/AnkieterLogIn/AnkieterLogIn.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, ButtonGroup, FormLabel, Input } from "@chakra-ui/react";
 import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const AnkieterLogIn = ({ login, passwd, change }) => {
+  const history = useHistory();
+  const user  = useAuth0();
+
+  useEffect(() => {
+    console.log(user);
+
+    if(!user.isLoading && !user.isAuthenticated){
+      history.push('/login');
+    }
+  }, [user])
+
   return (
     <div className={"Ankieterlogin"}>
       <FormLabel>Login: </FormLabel>
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      redirectUri={window.location.origin}
+      redirectUri={"http://localhost:3000/login/ankieter"}
     >
       <App />
     </Auth0Provider>
