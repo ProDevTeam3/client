d364cf160a1afe36d892fbe70d12f453fc60b3e0
Robert-Jankowski <Robert-Jankowski@users.noreply.github.com>
2021-04-19 10:22:29 +0000

    Prettified Code!

--- src/charts/StackedBarChart/exampleInput.js
+++ src/charts/StackedBarChart/exampleInput.js
@@ -1,87 +1,87 @@
 const data = [
+  {
+    label: "polska",
+    values: [
       {
-        label: "polska",
-        values: [
-          {
-            name: "Gastronomia",
-            value: 623,
-          },
-          {
-            name: "Górnictwo i wydobywanie",
-            value: 823,
-          },
-          {
-            name: "Budownictwo",
-            value: 932,
-          },
-        ],
+        name: "Gastronomia",
+        value: 623,
       },
       {
-        label: "ukraińska",
-        values: [
-          {
-            name: "Gastronomia",
-            value: 234,
-          },
-          {
-            name: "Górnictwo i wydobywanie",
-            value: 235,
-          },
-          {
-            name: "Budownictwo",
-            value: 327,
-          },
-        ],
+        name: "Górnictwo i wydobywanie",
+        value: 823,
       },
       {
-        label: "niemiecka",
-        values: [
-          {
-            name: "Gastronomia",
-            value: 274,
-          },
-          {
-            name: "Górnictwo i wydobywanie",
-            value: 215,
-          },
-          {
-            name: "Budownictwo",
-            value: 327,
-          },
-        ],
+        name: "Budownictwo",
+        value: 932,
+      },
+    ],
+  },
+  {
+    label: "ukraińska",
+    values: [
+      {
+        name: "Gastronomia",
+        value: 234,
       },
       {
-        label: "litewska",
-        values: [
-          {
-            name: "Gastronomia",
-            value: 34,
-          },
-          {
-            name: "Górnictwo i wydobywanie",
-            value: 123,
-          },
-          {
-            name: "Budownictwo",
-            value: 53,
-          },
-        ],
+        name: "Górnictwo i wydobywanie",
+        value: 235,
       },
       {
-        label: "rosyjska",
-        values: [
-          {
-            name: "Gastronomia",
-            value: 122,
-          },
-          {
-            name: "Górnictwo i wydobywanie",
-            value: 333,
-          },
-          {
-            name: "Budownictwo",
-            value: 97,
-          },
-        ],
+        name: "Budownictwo",
+        value: 327,
       },
-    ];
+    ],
+  },
+  {
+    label: "niemiecka",
+    values: [
+      {
+        name: "Gastronomia",
+        value: 274,
+      },
+      {
+        name: "Górnictwo i wydobywanie",
+        value: 215,
+      },
+      {
+        name: "Budownictwo",
+        value: 327,
+      },
+    ],
+  },
+  {
+    label: "litewska",
+    values: [
+      {
+        name: "Gastronomia",
+        value: 34,
+      },
+      {
+        name: "Górnictwo i wydobywanie",
+        value: 123,
+      },
+      {
+        name: "Budownictwo",
+        value: 53,
+      },
+    ],
+  },
+  {
+    label: "rosyjska",
+    values: [
+      {
+        name: "Gastronomia",
+        value: 122,
+      },
+      {
+        name: "Górnictwo i wydobywanie",
+        value: 333,
+      },
+      {
+        name: "Budownictwo",
+        value: 97,
+      },
+    ],
+  },
+];
