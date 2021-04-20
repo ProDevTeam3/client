38ca5cb3c136842ff84185563dcc59330494f5c8
Kacper Kurz <kacperkurz@protonmail.com>
2021-04-20 18:49:21 +0200

    SPIS-135 Dodadno podstawowy koncept

---
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -0,0 +1,17 @@
+import { Box, Center } from "@chakra-ui/react"
+import React from "react";
+
+const AdminPanel = () =>{
+    return(
+        <Center  height="100vh" width="100vw">
+            <Box>
+                {/*PanelNavBar*/}
+            </Box>
+            <Box>
+                {/*PanelMain*/}
+            </Box>
+        </Center>
+    )
+}
+
+export default AdminPanel
