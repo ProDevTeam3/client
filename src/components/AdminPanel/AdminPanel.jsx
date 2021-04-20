216d190e5ee4ef2705745730d604001a5eedc484
Robert <robert.jankowski.kontakt@gmail.com>
2021-04-20 22:18:33 +0200

    wybaczcie mi albowiem pushuje na developa

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,16 +1,16 @@
-import { Box, Center } from "@chakra-ui/react"
+import { Box, Stack } from "@chakra-ui/react"
 import React from "react";
 
 const AdminPanel = () =>{
     return(
-        <Center  height="100vh" width="100vw">
+        <Stack height="100vh" width="100vw">
             <Box height="15%" width="100%">
                 {/*PanelNavBar*/}
             </Box>
             <Box  height="85%" width="100%">
                 {/*PanelMain*/}
             </Box>
-        </Center>
+        </Stack>
     )
 }
 
