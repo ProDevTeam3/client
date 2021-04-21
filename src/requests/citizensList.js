1dcedef1318e118de53b1de8546a5847b9bbc217
marekpolom <marekpolom@users.noreply.github.com>
2021-04-21 12:19:29 +0000

    Prettified Code!

--- src/requests/citizensList.js
+++ src/requests/citizensList.js
@@ -1,28 +1,37 @@
 import axios from "axios";
 
 const getAllCitizens = async (page, limit) => {
-    return await axios.get(`http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/citizen/getCitizens`, {params:{
-        page: page,
-        limit: limit
-    }})
+  return await axios
+    .get(
+      `http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/citizen/getCitizens`,
+      {
+        params: {
+          page: page,
+          limit: limit,
+        },
+      }
+    )
     .then((res) => {
-        // console.log(res);
-        return res.data;
+      // console.log(res);
+      return res.data;
     })
     .catch((err) => {
-        console.log(err);
-    })
-}
+      console.log(err);
+    });
+};
 
 const getCitizen = async (pesel) => {
-    return await axios.get(`http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/citizen/getCitizen/${pesel}`)
+  return await axios
+    .get(
+      `http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/citizen/getCitizen/${pesel}`
+    )
     .then((res) => {
-        // console.log(res);
-        return res.data;
+      // console.log(res);
+      return res.data;
     })
     .catch((err) => {
-        console.log(err);
-    })
-}
+      console.log(err);
+    });
+};
 
-export {getAllCitizens, getCitizen};
+export { getAllCitizens, getCitizen };
