19591c9b25465a4f381e7787c3da5c9600d4b7fa
Marek Połom <marekpolom22@gmail.com>
2021-04-21 14:14:59 +0200

    SPIS-43 | wyszukiwanie użytkownika po peselu i małe poprawki

--- src/components/Citizens/CitizensList.jsx
+++ src/components/Citizens/CitizensList.jsx
@@ -16,43 +16,42 @@ import {
   ChevronLeftIcon,
   SearchIcon,
   CheckIcon,
+  SmallCloseIcon,
 } from "@chakra-ui/icons";
 import { getAllCitizens, getCitizen } from "../../requests/citizensList";
-import {
-  Link,
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // DANE TESTOWE
-const c = {
-  citizens: [
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-    { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
-  ],
-  size: 25,
-};
+// const c = {
+//   citizens: [
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//     { name: "Marek", lastName: "Kowalski", pesel: "12345678900" },
+//   ],
+//   size: 25,
+// };
 //
 
 const pageSize = 25;
@@ -61,31 +60,52 @@ const CitizensList = () => {
   const [isPreviousDisabled, isPreviousDisabledUpd] = useState(true);
   const [isNextDisabled, isNextDisabledUpd] = useState(false);
   const [currentPage, currentPageUpd] = useState(1);
+  const [currentPageMem, currentPageMemUpd] = useState(1);
   const [search, searchUpd] = useState("");
-  const [citizens, citizensUpd] = useState({citizens: [], size: 0});
+  const [citizens, citizensUpd] = useState({ citizens: [], size: 1 });
 
   const getCit = async () => {
     // !!! DODAJ KOMENTARZ ŻEBY POBIERAĆ DANE Z API !!!
-    const data =  c;
+    // const data =  c;
     //
 
     // !!! USUŃ KOMENTARZ ŻEBY POBIERAĆ DANE Z API !!!
-    // const data =  await getAllCitizens(currentPage, pageSize);
+    const data = await getAllCitizens(currentPage - 1, pageSize);
     //
 
-    if(data !== 'Nie znaleziono obywateli'){
+    if (data !== "Nie znaleziono obywateli") {
       citizensUpd(data);
-    }else{
-      citizensUpd({citizens: [], size: 0});
+    } else {
+      citizensUpd({ citizens: [], size: 1 });
     }
+  };
 
-    console.log(citizens);
-  }
+  const getCitByPesel = async (pesel) => {
+    const data = await getCitizen(pesel);
+
+    if (data !== `Nie znaleziono obywatela o PESELU: ${pesel}`) {
+      citizensUpd({
+        citizens: [
+          {
+            _id: data._id,
+            PESEL: data.PESEL,
+            first_name: data.first_name,
+            surname: data.surname,
+          },
+        ],
+        size: 1,
+      });
+    } else {
+      citizensUpd({ citizens: [], size: 1 });
+    }
+  };
 
   useEffect(() => {
     if (
       (currentPage === 1 &&
-      currentPage >= Math.ceil(citizens.size / pageSize)) || citizens.citizens.length === 0
+        currentPage >= Math.ceil(citizens.size / pageSize)) ||
+      citizens.citizens.length === 0 ||
+      search.toString().length === 11
     ) {
       isPreviousDisabledUpd(true);
       isNextDisabledUpd(true);
@@ -99,30 +119,44 @@ const CitizensList = () => {
       isNextDisabledUpd(false);
       isPreviousDisabledUpd(false);
     }
-
-    getCit();
-  }, [currentPage, isPreviousDisabled]);
+  }, [currentPage, isPreviousDisabled, citizens]);
 
   useEffect(() => {
-    if(search.toString().length === 11){
-      
+    if (
+      !(currentPage < 1) &&
+      (!(currentPage > Math.ceil(citizens.size / pageSize)) ||
+        Math.ceil(citizens.size / pageSize) === 0)
+    ) {
+      getCit();
+    }
+  }, [currentPage]);
+
+  useEffect(() => {
+    if (search.toString().length === 11) {
+      getCitByPesel(search.toString());
+    } else if (citizens.citizens.length <= 1 && !isNaN(parseInt(currentPage))) {
+      getCit();
     }
   }, [search]);
 
   const goToPreviousPage = () => {
     currentPageUpd(parseInt(currentPage) - 1);
+    searchUpd("");
   };
 
   const goToFirstPage = () => {
     currentPageUpd(1);
+    searchUpd("");
   };
 
   const goToNextPage = () => {
     currentPageUpd(parseInt(currentPage) + 1);
+    searchUpd("");
   };
 
   const goToLastPage = () => {
     currentPageUpd(Math.ceil(citizens.size / pageSize));
+    searchUpd("");
   };
 
   return (
@@ -135,7 +169,7 @@ const CitizensList = () => {
       flexDir="column"
     >
       <Center
-        width={{md: "80%", base: "94%"}}
+        width={{ md: "80%", base: "94%" }}
         minH="73vh"
         maxH="73vh"
         display="flex"
@@ -164,10 +198,20 @@ const CitizensList = () => {
             fontSize="1.1em"
             textAlign="center"
           >
-            <Box d="flex" flexDir={{sm: "row", base: "column"}} justifyContent="space-between">
-              <Box width="180px" display={{sm: "none", md: "block", base: "none"}}></Box>
+            <Box
+              d="flex"
+              flexDir={{ sm: "row", base: "column" }}
+              justifyContent="space-between"
+            >
+              <Box
+                width="180px"
+                display={{ sm: "none", md: "block", base: "none" }}
+              ></Box>
               <Center color="white">Lista Obywateli</Center>
-              <InputGroup width={{sm: "180px", base: "none"}} marginTop={{sm: "0", base: "10px"}}>
+              <InputGroup
+                width={{ sm: "180px", base: "none" }}
+                marginTop={{ sm: "0", base: "10px" }}
+              >
                 <InputLeftElement
                   pointerEvents="none"
                   children={<SearchIcon color="gray.300" />}
@@ -185,9 +229,30 @@ const CitizensList = () => {
                     } else {
                       searchUpd(parseInt(e.target.value));
                     }
+
+                    if (e.target.value.length === 11) {
+                      getCitByPesel(e.target.value);
+                    }
                   }}
                 />
-                <InputRightElement children={<CheckIcon color={(search.toString().length === 11 && citizens.citizens.length === 1) ? "green.400" : "gray.300"} />} />
+                {search.toString().length === 11 &&
+                  citizens.citizens.length === 1}
+                <InputRightElement
+                  children={
+                    search.toString().length === 11 &&
+                    citizens.citizens.length === 1 ? (
+                      <CheckIcon color="green.400" transition="0.2s" _hover={{color: "green.500"}} _active={{color: "green.600"}}/>
+                    ) : search.toString().length === 11 &&
+                      citizens.citizens.length === 0 ? (
+                      <SmallCloseIcon color="red.400" fontSize="1.4em" transition="0.2s" _hover={{color: "red.500"}} _active={{color: "red.600"}}/>
+                    ) : (
+                      <SmallCloseIcon color="white" />
+                    )
+                  }
+                  onClick={(e) => {
+                    searchUpd('');
+                  }}
+                />
               </InputGroup>
             </Box>
           </Box>
@@ -198,82 +263,94 @@ const CitizensList = () => {
           padding={{ base: "0 5%", lg: "0 10%" }}
           height="80vh"
         >
-          {(citizens.citizens.length > 0) ?
-          citizens.citizens.map((item, i) => {
-            return (
-              <Link to={`/admin/citizens/${item.pesel}`}>
-              <Grid
-                key={i}
-                templateColumns="repeat(3, 1fr)"
-                textAlign="center"
-                width="100%"
-                borderRadius="5px"
-                fontSize={{md: "1.3em", base: "1em"}}
-                padding="8px 0"
-                margin="0.7em 0"
-                bg="gray.100"
-                boxShadow="md"
-                transition="box-shadow 0.2s, background-color 0.05s"
-                cursor="pointer"
-                _hover={{
-                  boxShadow: "lg",
-                }}
-                _active={{
-                  bg: "gray.200"
-                }}
-                d='flex'
-                justifyContent='space-around'
-                flexDir="row"
-                flexWrap="wrap"
-                onClick={(e) => {
-                  
-                }}
-              >
-                <Box d="flex" flexDir="row" padding={{base: "0 10px", sm: "0"}}>
-                  <Box
-                    fontSize={{md: "0.6em", sm: "0.5em", base: "0.6em"}}
-                    fontWeight="bold"
-                    color="gray.400"
-                    lineHeight="3em"
+          {citizens.citizens.length > 0 ? (
+            citizens.citizens.map((item, i) => {
+              return (
+                <Link to={`/admin/citizens/${item.PESEL}`}>
+                  <Grid
+                    key={i}
+                    templateColumns="repeat(3, 1fr)"
+                    textAlign="center"
+                    width="100%"
+                    borderRadius="5px"
+                    fontSize={{ md: "1.3em", base: "1em" }}
+                    padding="8px 0"
+                    margin="0.7em 0"
+                    bg="gray.100"
+                    boxShadow="md"
+                    transition="box-shadow 0.2s, background-color 0.05s"
+                    cursor="pointer"
+                    _hover={{
+                      boxShadow: "lg",
+                    }}
+                    _active={{
+                      bg: "gray.200",
+                    }}
+                    d="flex"
+                    justifyContent="space-around"
+                    flexDir="row"
+                    flexWrap="wrap"
+                    onClick={(e) => {}}
                   >
-                    PESEL&nbsp;
-                  </Box>
-                  <Box>{item.pesel}</Box>
-                </Box>
-                <Box
-                  d="flex"
-                  flexDir="row"
-                  padding={{base: "0 10px", sm: "0"}}
-                >
-                  <Box
-                    fontSize={{md: "0.6em", sm: "0.5em", base: "0.6em"}}
-                    fontWeight="bold"
-                    color="gray.400"
-                    lineHeight="3em"
-                  >
-                    IMIĘ&nbsp;
-                  </Box>
-                  <Box>{item.name}</Box>
-                </Box>
-                <Box d="flex" flexDir="row" padding={{base: "0 10px", sm: "0"}}>
-                  <Box
-                    fontSize={{md: "0.6em", sm: "0.5em", base: "0.6em"}}
-                    fontWeight="bold"
-                    color="gray.400"
-                    lineHeight={{md: "3em", base: "3em"}}
-                  >
-                    NAZWISKO&nbsp;
-                  </Box>
-                  <Box>{item.lastName}</Box>
-                </Box>
-              </Grid>
-              </Link>
-            );
-          })
-        :
-          <Center height="100%" fontSize='2em' color='gray.300' fontWeight="bold">
-            Nie znaleziono obywateli
-          </Center>}
+                    <Box
+                      d="flex"
+                      flexDir="row"
+                      padding={{ base: "0 10px", sm: "0" }}
+                    >
+                      <Box
+                        fontSize={{ md: "0.6em", sm: "0.5em", base: "0.6em" }}
+                        fontWeight="bold"
+                        color="gray.400"
+                        lineHeight="3em"
+                      >
+                        PESEL&nbsp;
+                      </Box>
+                      <Box>{item.PESEL}</Box>
+                    </Box>
+                    <Box
+                      d="flex"
+                      flexDir="row"
+                      padding={{ base: "0 10px", sm: "0" }}
+                    >
+                      <Box
+                        fontSize={{ md: "0.6em", sm: "0.5em", base: "0.6em" }}
+                        fontWeight="bold"
+                        color="gray.400"
+                        lineHeight="3em"
+                      >
+                        IMIĘ&nbsp;
+                      </Box>
+                      <Box>{item.first_name}</Box>
+                    </Box>
+                    <Box
+                      d="flex"
+                      flexDir="row"
+                      padding={{ base: "0 10px", sm: "0" }}
+                    >
+                      <Box
+                        fontSize={{ md: "0.6em", sm: "0.5em", base: "0.6em" }}
+                        fontWeight="bold"
+                        color="gray.400"
+                        lineHeight={{ md: "3em", base: "3em" }}
+                      >
+                        NAZWISKO&nbsp;
+                      </Box>
+                      <Box>{item.surname}</Box>
+                    </Box>
+                  </Grid>
+                </Link>
+              );
+            })
+          ) : (
+            <Center
+              height="100%"
+              fontSize="2em"
+              color="gray.300"
+              fontWeight="bold"
+            >
+              Nie znaleziono obywateli
+            </Center>
+          )}
         </Box>
       </Center>
       <Center paddingTop="2.5vh">
@@ -311,18 +388,19 @@ const CitizensList = () => {
                 currentPageUpd("");
               } else {
                 currentPageUpd(parseInt(e.target.value));
-                // getCit();
               }
+
+              searchUpd("");
+            }}
+            onFocus={(e) => {
+              currentPageMemUpd(currentPage);
             }}
             onBlur={(e) => {
               if (isNaN(parseInt(e.target.value)) || e.target.value < 1) {
-                currentPageUpd(1);
-              } else if (
-                e.target.value > Math.ceil(citizens.size / pageSize)
-              ) {
+                currentPageUpd(currentPageMem);
+              } else if (e.target.value > Math.ceil(citizens.size / pageSize)) {
                 currentPageUpd(Math.ceil(citizens.size / pageSize));
               }
-              console.log("REQUEST");
             }}
           />
           <IconButton
