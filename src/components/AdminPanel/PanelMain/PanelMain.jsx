458a173c8dedc864848108efb8477fdd6b6c3317
Robert <robert.jankowski.kontakt@gmail.com>
2021-04-22 13:59:47 +0200

    Poprawa pathy w PanelMain

--- src/components/AdminPanel/PanelMain/PanelMain.jsx
+++ src/components/AdminPanel/PanelMain/PanelMain.jsx
@@ -1,7 +1,6 @@
 import {Switch, Route, useRouteMatch, Redirect} from "react-router-dom";
 import React from "react";
 import Statistics from "../Statistics/Statistics"
-import EditView from "../EditView/EditView";
 import CitizensList from "../CitizenList/CitizensList";
 
 const PanelMain = () =>{
@@ -13,14 +12,11 @@ const PanelMain = () =>{
             <Route exact path={path}>
                 <Redirect to={path + "/citizenslist"} />
             </Route>
-            <Route path={path + "/statistics"}>
+            <Route exact path={path + "/statistics"}>
                 <Statistics/>
             </Route>
-            <Route path={path + "/citizenslist"}>
+            <Route exact path={path + "/citizenslist"}>
                 <CitizensList/>
-            </Route>
-            <Route path={path + "/editview"}>
-                <EditView/>
             </Route>
         </Switch>
     )
