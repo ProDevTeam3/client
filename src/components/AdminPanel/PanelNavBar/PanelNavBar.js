737fa432a4c299fcc9150cb0108accec5735d48e
Kamil Bakierzynski <bakierzynskikamil@gmail.com>
2021-04-22 15:03:40 +0200

    Fix dla navbara < 900px

--- src/components/AdminPanel/PanelNavBar/PanelNavBar.js
+++ src/components/AdminPanel/PanelNavBar/PanelNavBar.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { Box, Heading, Flex, Text, Button, Link } from "@chakra-ui/react";
+import React from "react"
+import { Box, Heading, Flex, Text, Button, Link, useMediaQuery, IconButton, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Link as ReachLink } from "react-router-dom";
+import { Link as ReachLink, useHistory } from "react-router-dom";
 
 const MenuItems = ({ children }) => (
   <Text mt={{ base: 4, md: 0 }} mr={2} display="block">
@@ -13,18 +14,23 @@ const PanelNavBar = (props) => {
   const { logout } = useAuth0();
   const [show, setShow] = React.useState(false);
   const handleToggle = () => setShow((oldState) => !oldState);
-  return (
-    <Flex
+  const [isLargerThan900] = useMediaQuery("(min-width: 900px)");
+  const history = useHistory();
+  return isLargerThan900 ?
+    (<Flex
       as="nav"
       align="center"
       justify="space-between"
       wrap="wrap"
-      padding="1.5rem"
+      paddingLeft="30px"
+      paddingRight="30px"
       bg="teal.500"
       color="white"
       height="100%"
+      width="100%"
     >
-      <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
+      < Box display={{ base: "block", md: "none" }
+      } onClick={handleToggle} >
         <svg
           fill="white"
           width="12px"
@@ -34,7 +40,7 @@ const PanelNavBar = (props) => {
           <title>Menu</title>
           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
         </svg>
-      </Box>
+      </Box >
 
       <Box
         display={{ sm: show ? "block" : "none", md: "flex" }}
@@ -85,8 +91,30 @@ const PanelNavBar = (props) => {
           Wyloguj
         </Button>
       </Box>
-    </Flex>
-  );
+    </Flex >)
+    : (<Menu>
+      <MenuButton
+        as={IconButton}
+        aria-label="Options"
+        icon={<HamburgerIcon />}
+        colorScheme="gray"
+        marginLeft="30px"
+      />
+      <MenuList zIndex={1000}>
+        <MenuItem onClick={() => history.push("/admin/citizenslist")}>
+          Lista osób
+      </MenuItem>
+        <MenuItem onClick={() => history.push("/form")}>
+          Formularz
+      </MenuItem>
+        <MenuItem onClick={() => history.push("/admin/statistics")}>
+          Statystyki
+      </MenuItem>
+        <MenuItem onClick={() => logout({ returnTo: window.location.origin })}>
+          Wyloguj
+      </MenuItem>
+      </MenuList>
+    </Menu>);
 };
 
 export default PanelNavBar;
