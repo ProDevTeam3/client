afe13fac8cbf7797db932a90b8d78bfef0977a26
Marek Połom <marekpolom22@gmail.com>
2021-04-22 12:58:26 +0200

    SPIS-18 | autoryzacja przed wejściem do panelu admina

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,19 +1,29 @@
 import { Box, Stack } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
 import PanelNavBar from "./PanelNavBar/PanelNavBar";
 import PanelMain from "./PanelMain/PanelMain";
+import NoPermission from "../NoPermission/NoPermission";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const AdminPanel = () =>{
-    return(
-        <Stack width="100vw">
-            <Box height="12vh" width="100%">
-                <PanelNavBar/>
-            </Box>
-            <Box style={{marginTop:"0"}} height="88vh" width="100%">
-                <PanelMain/>
-            </Box>
-        </Stack>
-    )
+    const { user, isLoading, isAuthenticated } = useAuth0();
+
+    if (isAuthenticated && !isLoading && user["https://prodevteam-spis.com/authorization"].groups.includes("Admin")){
+        return(
+            <Stack width="100vw">
+                <Box height="12vh" width="100%">
+                    <PanelNavBar/>
+                </Box>
+                <Box style={{marginTop:"0"}} height="88vh" width="100%">
+                    <PanelMain/>
+                </Box>
+            </Stack>
+        )
+    }else{
+        return(
+            <NoPermission/>
+        )
+    }
 }
 
 export default AdminPanel;
