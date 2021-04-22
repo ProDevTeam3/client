28a081ce11a8f5e3582355b469c701f54697bbe2
Marek Połom <marekpolom22@gmail.com>
2021-04-22 12:59:12 +0200

    SPIS-18 | strona, które wyświetla się po błędnej autoryzacji

---
+++ src/components/NoPermission/NoPermission.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Center, Box, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+const NoPermission = () => {
+    return(
+        <Center bg='gray.100' height='100vh' width='100vw' d='flex' flexDir='column'>
+            <Box fontSize='3em' fontWeight="600" color='gray.500'>
+                Brak wymaganych uprawnień!
+            </Box>
+            <Link to="/login">
+                <Button color='white' marginTop="4em" bg='teal.400' _hover={{bg: "teal.500"}}>
+                    Powrót do strony logowania
+                </Button>
+            </Link>
+        </Center>
+    );
+}
+
+export default NoPermission;
